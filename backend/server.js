d55c081d952dcf922295627e543e410d48de1b81
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-05-18 11:08:35 +0530

    mongo uri unified

--- backend/server.js
+++ backend/server.js
@@ -17,15 +17,13 @@ app.use('/api', contactRoutes);
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => {
     console.error('❌ MongoDB connection failed:', err.message);
     process.exit(1);
   });
+
 
 // Root route (optional)
 app.get('/', (req, res) => {
